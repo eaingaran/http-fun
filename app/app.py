3b899af8831b4cbffe075244b40f0845e29c9573
Aingaran <eaingaran@gmail.com>
2021-09-28 13:47:37 +0200

    Added endpoints for other services and configured 404

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, make_response
 import re
 
 
@@ -8,7 +8,18 @@ app = Flask(__name__)
 @app.route('/helloworld')
 def hello_world():
     name = request.args.get('name')
-    return 'Hello Stranger' if name is None else 'Hello ' + ' '.join(re.findall('[A-Z][^A-Z]*', name))
+    return 'Hello Stranger' if name is None else 'Hello ' + ' '.join(re.findall('[A-Z][^A-Z]*', name)), 200
+
+
+@app.route('/versionz')
+def version_info():
+    info = {'git hash': '291250b2f59ce91f440c3261da3344192e6911c3', 'name': 'http-fun'}
+    return jsonify(info), 200
+
+
+@app.errorhandler(404)
+def handle_404(e):
+    return jsonify({'Status Code': 404, 'Message': str(e)}), 200
 
 
 def say_hello():
